f5776b5b0376b9a822c132127ea8ba618b4730d6
Hariririski <hariririski@gmail.com>
2020-04-10 23:19:02 +0700

    alpha2

--- application/controllers/Umum_Home.php
+++ application/controllers/Umum_Home.php
@@ -4,23 +4,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
      function __construct() {
          parent::__construct();
-  			 $this->load->helper('url');
-  			 $this->load->library('session');
-  			 $this->load->database();
-         //$this->load->model('M_lapor');
-         $this->load->model('M_lapor_hp');
-         //$this->load->model('M_info');
+  			 // $this->load->helper('url');
+  			 // $this->load->library('session');
+  			 // $this->load->database();
+         // $this->load->model('M_lapor');
+         // $this->load->model('M_lapor_hp');
+         // $this->load->model('M_info');
   		}
 
 
          public function index()
        	{
           echo a;
-          $data['jumlah_pengaduan'] = $this->M_lapor_hp->jumlah_pengaduan();
-          $data['pengaduan_selesai'] = $this->M_lapor_hp->pengaduan_selesai();
-          $data['pengaduan_proses'] = $this->M_lapor_hp->pengaduan_proses();
-          $data['jumlah_informasi'] = $this->M_lapor_hp->jumlah_informasi();
-          $data['max5'] = $this->M_lapor->max5();
+          // $data['jumlah_pengaduan'] = $this->M_lapor_hp->jumlah_pengaduan();
+          // $data['pengaduan_selesai'] = $this->M_lapor_hp->pengaduan_selesai();
+          // $data['pengaduan_proses'] = $this->M_lapor_hp->pengaduan_proses();
+          // $data['jumlah_informasi'] = $this->M_lapor_hp->jumlah_informasi();
+          // $data['max5'] = $this->M_lapor->max5();
 
           //
           // $query1=$this->db->query("SELECT * FROM `jenis_informasi`");
