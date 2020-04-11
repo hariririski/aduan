2f4dad0a13d7a84780a8b860f7f865aaf25befca
Hariririski <hariririski@gmail.com>
2020-04-11 22:35:32 +0700

    Update Hp.php

--- application/controllers/Hp.php
+++ application/controllers/Hp.php
@@ -41,7 +41,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       {
         $no_telepon=$this->uri->segment(3);
         if(isset($no_telepon)){
-
+            $no_telepon = $this->uri->segment(3);
+            $data['data_pengaduan'] = $this->M_lapor_hp->data_pengaduan($no_telepon);
+            $this->load->view('hp/cek_aduan',$data);
         }else{
           $this->load->view('hp/cek_aduan');
         }
