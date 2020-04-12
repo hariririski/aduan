7fce18d877ae68f1cb5d432ae088e14e3bd1824f
Hariririski <hariririski@gmail.com>
2020-04-12 15:20:17 +0700

    Table

--- application/views/share/table_header.php
+++ application/views/share/table_header.php
@@ -1,8 +1,8 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="<?php echo site_url(); ?>assets/umum/jquery.min.js"></script>
   <link type="text/css" rel="stylesheet" href="<?php echo site_url(); ?>assets/umum/bootstrap.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="<?php echo site_url(); ?>assets/umum/bootstrap.min.js"></script>
 
   <!--Script CSS-->
-  <link type="text/css" href='https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css' rel='stylesheet'>
-  <link type="text/css" href='https://cdn.datatables.net/responsive/2.2.1/css/responsive.dataTables.min.css' rel='stylesheet'>
+  <link type="text/css" href='<?php echo site_url(); ?>assets/umum/jquery.dataTables.min.css' rel='stylesheet'>
+  <link type="text/css" href='<?php echo site_url(); ?>assets/umum/responsive.dataTables.min.css' rel='stylesheet'>
   <link type="text/css" href='<?php echo site_url(); ?>assets/umum/buttons.dataTables.min.css' rel='stylesheet'>
