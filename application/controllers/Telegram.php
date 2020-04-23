6bc93dfae951f11a5e31f6c6a309a5078632212d
Hariririski <hariririski@gmail.com>
2020-04-23 12:00:40 +0700

    Update Telegram.php

--- application/controllers/Telegram.php
+++ application/controllers/Telegram.php
@@ -26,18 +26,22 @@ class Telegram extends CI_Controller {
     $chat_id = $updates[message][chat][id];
     $pesan = strtoupper($pesan);
 
-    //$pesan="LAPOR#111111111111#nama#222222222#alamat#pekerjaan#akumau atau tentang dirimu";
-    //$chat_id="-343349381";
+    $pesan="LAPOR#111111111111#nama#222222222#alamat#pekerjaan#akumau atau tentang dirimu";
+    $chat_id="-343349381";
     if(strpos($pesan,"APOR#")>0){
       $datas = split("#",$pesan);
+      print_r($datas);
       $nik = $datas[1];
       $nama = $datas[2];
       $no_hp = $datas[3];
       $alamat = $datas[4];
       $pekerjaan = $datas[5];
       $uraian = $datas[6];
+      $tanggal = date("Y-m-d");
       $kirim= $this->M_telegram->add($nik,$nama,$no_hp,$alamat,$pekerjaan,$uraian);
-      $pesan_balik="Terimakasih";
+      echo $kirim;
+      $media="Telegram";
+      $pesan_balik="Pengaduan! %0ANama%20%20%20%20%20:%20$nama%0ATanggal%20%20:%20$tanggal  %0AHP%20%20%20%20%20%20%20%20%20%20%20:%20$no_hp %0AMedia%20%20%20%20%20:%20$media %0AUraian%20%20%20%20%20:%0A%20%20%20%20%20$uraian%0A%0A Terima Kasih Atas Pengaduan Anda%0A%0A Pengaduan Anda Secara Otomatis Masuk Kedalam Aplikasi KIBAN BPN KOTA BANDA ACEH %0A%0A Untuk Pemantauan Pengaduan Dengan Mengirim Format: %0ACEK#$kirim";
 
     }
     else if(strpos($pesan,"APORAN#")>0)
@@ -132,7 +136,7 @@ class Telegram extends CI_Controller {
     else {
       $pesan_balik = "Format Lapor Pengaduan LAPOR%23[NIK]%23[NAMA]%23[HP]%23[ALAMAT]%23[PEKERJAAN]%23[URAIAN PENGADUAN] %0A Contoh LAPOR%231171245708900001%23Antini%23082276226790%23Setui%23PNS%23Assammualaikum......Terima Kasih ";
     }
-    $API = "https://api.telegram.org/$token/sendMessage?parse_mode=html&chat_id=$chat_id&text=$pesan_balik";
+    echo$API = "https://api.telegram.org/$token/sendMessage?parse_mode=html&chat_id=$chat_id&text=$pesan_balik";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
